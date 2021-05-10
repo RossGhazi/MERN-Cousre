e78a04675f67481c35afd794cb6a9d56011d46c6
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-05-10 09:22:40 -0600

    end of 10

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
 
-router.get("/", (req, res) => res.send("users  route"));
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "please include valid email adress").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or mrore charechters"
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("users route");
+  }
+);
 
 module.exports = router;
