62447d9c58558df6e642c3ae7d9aafedaa054e63
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-05-13 23:38:56 -0600

    server: end of 16 | client: end of 32

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("./../../middleware/auth");
+const Profile = require("./../../models/Profile");
+const User = require("./../../models/User");
 
-router.get("/", (req, res) => res.send("profile  route"));
+//@route GET api/profile/me
+//@desc    Get current user api
+//@access Private
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      "user",
+      ["avatar", "name"]
+    );
+    if (!profile) {
+      return res
+        .status(400)
+        .json({ mag: " There is no profile for this user " });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json("Server Error");
+  }
+});
 
 module.exports = router;
