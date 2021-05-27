520dfe2c0bf1090847dd0726d822d25b078e156c
Rouzbeh1797 <rouzbeh1797@gmail.com>
2021-05-27 13:00:29 -0600

    server: end of 22 | client: end of 33

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 const App = () => (
-  <>
-    <Navbar />
-    <Landing />
-  </>
+  <Router>
+    <>
+      <Navbar />
+      <Route exact path="/">
+        <Landing />
+      </Route>
+
+      <section className="container">
+        <Switch>
+          <Route exact path="/Register">
+            <Register />
+          </Route>
+          <Route exact path="/Login">
+            <Login />
+          </Route>
+        </Switch>
+      </section>
+    </>
+  </Router>
 );
 
 export default App;
